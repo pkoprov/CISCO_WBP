5ab1817b8090ee3046b95e5dd02a58292d4ff8b1
pkoprov <koprovp@gmail.com>
2022-02-15 18:24:26 -0500

    Var 'train' is renamed into 'kernel'

--- Convolution_UR-5.py
+++ Convolution_UR-5.py
@@ -3,14 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-
-k=0
 folders = ["UR-5e_1/wTool", "UR-5e_2"]
 for i, folder in enumerate(folders):
+    k=0
     for j, file in enumerate(os.listdir(folder)):
+        if k==2:
+            break
         if ".csv" in file:
             df = pd.read_csv(f"{folder}/{file}")
-            globals()[f"train{i}{j}"] = np.array(df.iloc[:,1])
+            globals()[f"kernel{i}{j}"] = np.array(df.iloc[:,1])
+            k+=1
             # plt.figure()
             # plt.plot(globals()[f"train{i}{j}"])
 
@@ -24,8 +26,7 @@ ax[0][0].set_title("UR-5e_1")
 k=0
 for i in range(2):
     for j in range(2):
-        ax[i][j].plot(globals()[f"train{j}{i}"], label="Kernel")
-        # plt.plot(globals()[f"train{i}{j}"], label=f"{label}")
+        ax[i][j].plot(globals()[f"kernel{j}{i}"], label="Kernel")
         k+=1
 
 
@@ -34,14 +35,15 @@ for folder in folders:
         if num in range(2):
             continue
         if ".csv" in file:
-            train_x = np.array(pd.read_csv(f"{folder}/{file}").iloc[:,1])
+            unknown = np.array(pd.read_csv(f"{folder}/{file}").iloc[:, 1])
 
             for i in range(2):
                 for j in range(2):
-                    a=np.convolve(np.flip(train_x),globals()[f"train{j}{i}"])
+                    a=np.convolve(np.flip(unknown), globals()[f"kernel{j}{i}"])
                     s[i,j]=max(a)
             S = np.max(s)
             s1 = s/S
             print("Looks like plot #", s1.argmax()+1)
-            ax[s1.argmax()//2][s1.argmax()%2].plot(train_x, label=f"{file}")
+            ax[s1.argmax()//2][s1.argmax()%2].plot(unknown, label=f"{file}")
             ax[s1.argmax()//2][s1.argmax()%2].legend()
+plt.show()
