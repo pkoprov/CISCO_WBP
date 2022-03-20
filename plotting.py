0e93e93821c0c7f7efdb959ff27920f898d46b1a
Pavel Koprov <koprovp@gmail.com>
2022-03-20 11:53:29 -0400

    cosmetic changes

--- plotting.py
+++ plotting.py
@@ -26,7 +26,6 @@ for folder in os.listdir(f"2022_02_28"):
             # plt.figure()
             # plt.plot(df.iloc[:,1])
 
-        #########################################
         match folder:
             case "270935":
                 mid = 2815
@@ -42,7 +41,6 @@ for folder in os.listdir(f"2022_02_28"):
                 mid = 2205
                 df5_ = reduced(df5,mid)
         #########################################
-        #########################################
             case "270936":
                 mid = 2645
                 df0_ = reduced(df0,mid)
@@ -57,7 +55,6 @@ for folder in os.listdir(f"2022_02_28"):
                 mid = 2535
                 df5_ = reduced(df5,mid)
         #########################################
-        #########################################
             case "270904":
                 mid = 2980
                 df0_ = reduced(df0,mid)
@@ -71,7 +68,6 @@ for folder in os.listdir(f"2022_02_28"):
                 df4_ = reduced(df4,mid)
                 mid = 2780
                 df5_ = reduced(df5,mid)
-        #########################################
         #########################################
             case "270328":
                 mid = 3300
@@ -91,8 +87,8 @@ for folder in os.listdir(f"2022_02_28"):
 
         # plt.figure()
         for i in range(6):
-            # plt.plot(globals()[f"df{i}_"])
-            plt.plot(globals()[f"df{i}_"][:, 1])
+            plt.plot(globals()[f"df{i}_"])
+            # plt.plot(globals()[f"df{i}_"][:, 1])
 
         if not os.path.exists(f"2022_02_28/{folder}_ex"):
             os.mkdir(f"2022_02_28/{folder}_ex")
@@ -101,7 +97,8 @@ for folder in os.listdir(f"2022_02_28"):
             pd.DataFrame(globals()[f"df{i}_"]).to_csv(f"2022_02_28/{folder}_ex/{file}", header=["X", "Y", "Z"], index=False)
 
 plt.figure()
-folder_ex = f"{folder}_ex"
-for i, file in enumerate(os.listdir(f"2022_02_28/{folder_ex}")):
-    df = pd.read_csv(f"2022_02_28/{folder_ex}/{file}")["X"]
-    plt.plot(df)
+for folder in os.listdir(f"2022_02_28"):
+    if "_ex" in folder:
+        for file in os.listdir(f"2022_02_28/{folder}"):
+            df = pd.read_csv(f"2022_02_28/{folder}/{file}")
+            plt.plot(df)
