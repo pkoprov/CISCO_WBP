4d86b4b9f0beac727bd735a35bef2318cde46035
Pavel Koprov <koprovp@gmail.com>
2022-03-19 20:53:24 -0400

    all is synchronized

--- plotting.py
+++ plotting.py
@@ -6,12 +6,19 @@ import numpy as np
 
 def reduced(df, med):
     lim = 3474/2
-    bound = [int(med-lim),int(med+lim)]
-    return df[bound[0]: bound[1]]
+    if med < lim:
+        zeros_quantity = int(lim - med)
+        bound = [0,int(med+lim)]
+        zero_df = pd.DataFrame([[0,9.8,1]]*zeros_quantity,columns=df.columns)
+        df = pd.concat([zero_df, df])
+    else:
+        bound = [int(med-lim),int(med+lim)]
+    answer = np.array(df[bound[0]: bound[1]])
+    return answer
 
 length = 3474
 
-folder = "270936"
+folder = "270328"
 for i, file in enumerate(os.listdir(f"2022_02_28/{folder}")):
     df = pd.read_csv(f"2022_02_28/{folder}/{file}").iloc[1:,1:4]
     globals()[f"df{i}"] = df
@@ -20,67 +27,81 @@ for i, file in enumerate(os.listdir(f"2022_02_28/{folder}")):
 
 
 #########################################
-# for 270904
-# mid = 2850
-# df0_ = np.array(reduced(df0,mid))
-# mid = 1980
-# df1_ = np.array(reduced(df1,mid))
-# mid = 2650
-# df2_ = np.array(reduced(df2,mid))
-# mid = 2820
-# df3_ = np.array(reduced(df3,mid))
-# mid = 2960
-# df4_ = np.array(reduced(df4,mid))
-# mid = 2240
-# df5_ = np.array(reduced(df5,mid))
+# for 270935
+# mid = 2815
+# df0_ = reduced(df0,mid)
+# mid = 1945
+# df1_ = reduced(df1,mid)
+# mid = 2615
+# df2_ = reduced(df2,mid)
+# mid = 2785
+# df3_ = reduced(df3,mid)
+# mid = 2925
+# df4_ = reduced(df4,mid)
+# mid = 2205
+# df5_ = reduced(df5,mid)
 #########################################
 #########################################
 # for 270936
-# mid = 2680
-# df0_ = np.array(reduced(df0,mid))
-# mid = 2970
-# df1_ = np.array(reduced(df1,mid))
-# mid = 2270
-# df2_ = np.array(reduced(df2,mid))
-# mid = 1780
-# df3_ = np.array(reduced(df3,mid))
-# mid = 2790
-# df4_ = np.array(reduced(df4,mid))
-# mid = 2570
-# df5_ = np.array(reduced(df5,mid))
+# mid = 2645
+# df0_ = reduced(df0,mid)
+# mid = 2935
+# df1_ = reduced(df1,mid)
+# mid = 2235
+# df2_ = reduced(df2,mid)
+# mid = 1745
+# df3_ = reduced(df3,mid)
+# mid = 2755
+# df4_ = reduced(df4,mid)
+# mid = 2535
+# df5_ = reduced(df5,mid)
 #########################################
 #########################################
 # for 270904
-# mid = 3050
-# df0_ = np.array(reduced(df0,mid))
-# mid = 2210
-# df1_ = np.array(reduced(df1,mid))
-# mid = 2820
-# df2_ = np.array(reduced(df2,mid))
-# mid = 1850
-# df3_ = np.array(reduced(df3,mid))
-# mid = 2760
-# df4_ = np.array(reduced(df4,mid))
-# mid = 2850
-# df5_ = np.array(reduced(df5,mid))
+# mid = 2980
+# df0_ = reduced(df0,mid)
+# mid = 2140
+# df1_ = reduced(df1,mid)
+# mid = 2750
+# df2_ = reduced(df2,mid)
+# mid = 1780
+# df3_ = reduced(df3,mid)
+# mid = 2690
+# df4_ = reduced(df4,mid)
+# mid = 2780
+# df5_ = reduced(df5,mid)
 #########################################
 #########################################
 # for 270328
-# df0_ = np.array(df0[1637:])
-# df1_ = np.array(df1[810:810+length])
-# df2_ = np.array(df2[70:70+length])
-# df3_ = np.array(df3[1325:1325+length])
-# df4_ = np.array(df4[555:555+length])
-# df5_ = np.array(df5[870:870+length])
+# mid = 3300
+# df0_ = reduced(df0,mid)
+# mid = 2470
+# df1_ = reduced(df1,mid)
+# mid = 1730
+# df2_ = reduced(df2,mid)
+# mid = 2990
+# df3_ = reduced(df3,mid)
+# mid = 2215
+# df4_ = reduced(df4,mid)
+# mid = 2530
+# df5_ = reduced(df5,mid)
 #########################################
+
 
 plt.figure()
 for i in range(6):
-    plt.plot(globals()[f"df{i}_"])
+    # plt.plot(globals()[f"df{i}_"])
+    plt.plot(globals()[f"df{i}_"][:, 1])
 
 if not os.path.exists(f"2022_02_28/{folder}_ex"):
     os.mkdir(f"2022_02_28/{folder}_ex")
 
-for file in os.listdir(f"2022_02_28/{folder}"):
-    pd.DataFrame(globals()[f"df{i}_"]).to_csv(f"2022_02_28/{folder}_ex/{file}.csv", header=["X", "Y", "Z"], index=False)
+for i, file in enumerate(os.listdir(f"2022_02_28/{folder}")):
+    pd.DataFrame(globals()[f"df{i}_"]).to_csv(f"2022_02_28/{folder}_ex/{file}", header=["X", "Y", "Z"], index=False)
 
+
+plt.figure()
+folder_ex = f"{folder}_ex"
+for i, file in enumerate(os.listdir(f"2022_02_28/{folder_ex}")):
+    df = pd.read_csv(f"2022_02_28/{folder_ex}/{file}")["X"]
+    plt.plot(df)
