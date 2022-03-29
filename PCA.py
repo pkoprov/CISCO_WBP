4e30bb70ca7c6d2908138123e1433fcc95d3e99a
Pavel "Pasha" Koprov <84463424+pkoprov@users.noreply.github.com>
2022-03-29 16:22:22 -0400

    PCS recalculated using same mu

--- PCA.py
+++ PCA.py
@@ -30,8 +30,17 @@ def getPCA(pos, comp_num):
     df = np.array(df)
     U,S,Vh = np.linalg.svd(df)
     manPCA = pd.DataFrame(np.matmul(df,Vh)).iloc[:,:comp_num]
-    return manPCA, Vh, df
+    return manPCA, Vh, mu
 
+
+def projectPC(pos, mu, Vh,comp_num = 2):
+    i = int(input(f"Pick df (0..3): {[str(n) + '). ' + key for n, key in enumerate(df_keys)]}"))
+    df = globals()[df_keys[i]].iloc[:, pos:pos + 6].transpose()
+    df = np.array(df.iloc[:, :-1].astype('float') - mu)
+    PCS = pd.DataFrame(np.matmul(df, Vh)).iloc[:, :comp_num]
+    return PCS
+
+plt.scatter(PCS[0],PCS[1], c = 'k')
 
 length = 3474
 plt.ion()
@@ -40,6 +49,8 @@ coord_list = ['all','x','y','z']
 # create global variables to store x,y,z and xyz data
 for i in range(4):
     globals()[f'df_UR5_{coord_list[i]}'] = pd.DataFrame()
+
+colors = ['r', 'g', 'b', 'k']
 
 for folder in os.listdir(f"2022_02_28"):
     if "_ex" in folder:
@@ -59,37 +70,77 @@ for coord in globals().keys():
     if "df_UR5_" in coord:
         df_keys.append(coord)
 
+PCA4_4, Vh4, mu4 = getPCA(0,2)
+PCA3_4 = projectPC(6,mu4,Vh4)
+
+plt.scatter(PCA4_4[0],PCA4_4[1], c = colors[0])
+plt.scatter(PCA3_4[0],PCA3_4[1], c = colors[1])
+
+
 for i, n in zip(range(4), [0,6,12,18]):
     globals()[f"PCA{i}"], globals()[f"Vh{i}"], globals()[f"df{i}"] = getPCA(n,2)
-colors = ['r', 'g', 'b', 'k']
+
+
+#############################################################
+df4 = df_UR5_x.iloc[:-1,0:6].astype('float').transpose()
+df3 = df_UR5_x.iloc[:-1,6:12].astype('float').transpose()
+df2 = df_UR5_x.iloc[:-1,12:18].astype('float').transpose()
+df1 = df_UR5_x.iloc[:-1,18:].astype('float').transpose()
 #############################################################
 # projecting on UR5_4 PC
 #############################################################
 fig()
 plt.title(f'Projection on 2 PC of UR5_4_X',fontsize = 20)
 
-plt.scatter(PCA0[0],PCA0[1], c = colors[0])
+mu = np.mean(df4, axis = 0)
 
-for i in [1,2,3]:
-    PCS = pd.DataFrame(np.matmul(globals()[f"df{i}"],Vh0)).iloc[:,:2]
-    plt.scatter(PCS[0],PCS[1], c = colors[i])
+df4_ = np.array(df4-mu)
+_,_,Vh = np.linalg.svd(df4_)
 
+PCS4 = pd.DataFrame(np.matmul(df4_,Vh)).iloc[:,:2]
+plt.scatter(PCS4[0],PCS4[1], c = colors[0])
+
+df3_ = np.array(df3-mu)
+PCS3 = pd.DataFrame(np.matmul(df3_,Vh)).iloc[:,:2]
+plt.scatter(PCS3[0],PCS3[1], c = colors[1])
+
+df2_ = np.array(df2-mu)
+PCS2 = pd.DataFrame(np.matmul(df2_,Vh)).iloc[:,:2]
+plt.scatter(PCS2[0],PCS2[1], c = colors[2])
+
+df1_ = np.array(df1-mu)
+PCS1 = pd.DataFrame(np.matmul(df1_,Vh)).iloc[:,:2]
+plt.scatter(PCS1[0],PCS1[1], c = colors[3])
 
 plt.legend(["UR5_4","UR5_3","UR5_2","UR5_1"])
+
 #############################################################
-# projecting on UR5_1 PC
+# projecting on UR5_3 PC
 #############################################################
 fig()
-plt.title(f'Projection on 2 PC of UR5_1_X',fontsize = 20)
+plt.title(f'Projection on 2 PC of UR5_3_X',fontsize = 20)
 
-plt.scatter(PCA3[0],PCA3[1], c = colors[3])
+mu = np.mean(df3, axis = 0)
+df3_ = np.array(df3-mu)
+_,_,Vh = np.linalg.svd(df3_)
 
-for i in [0,1,2]:
-    PCS = pd.DataFrame(np.matmul(globals()[f"df{i}"],Vh3)).iloc[:,:2]
-    plt.scatter(PCS[0],PCS[1], c = colors[i])
+df4_ = np.array(df4-mu)
+PCS4 = pd.DataFrame(np.matmul(df4_,Vh)).iloc[:,:2]
+plt.scatter(PCS4[0],PCS4[1], c = colors[0])
 
+df3_ = np.array(df3-mu)
+PCS3 = pd.DataFrame(np.matmul(df3_,Vh)).iloc[:,:2]
+plt.scatter(PCS3[0],PCS3[1], c = colors[1])
 
-plt.legend(["UR5_1","UR5_4","UR5_3","UR5_2"])
+df2_ = np.array(df2-mu)
+PCS2 = pd.DataFrame(np.matmul(df2_,Vh)).iloc[:,:2]
+plt.scatter(PCS2[0],PCS2[1], c = colors[2])
+
+df1_ = np.array(df1-mu)
+PCS1 = pd.DataFrame(np.matmul(df1_,Vh)).iloc[:,:2]
+plt.scatter(PCS1[0],PCS1[1], c = colors[3])
+
+plt.legend(["UR5_4","UR5_3","UR5_2","UR5_1"])
 
 #############################################################
 # projecting on UR5_2 PC
@@ -97,32 +148,54 @@ plt.legend(["UR5_1","UR5_4","UR5_3","UR5_2"])
 fig()
 plt.title(f'Projection on 2 PC of UR5_2_X',fontsize = 20)
 
-plt.scatter(PCA2[0],PCA2[1], c = colors[2])
+mu = np.mean(df2, axis = 0)
+df2_ = np.array(df2-mu)
+_,_,Vh = np.linalg.svd(df2_)
 
-for i in [0,1,3]:
-    PCS = pd.DataFrame(np.matmul(globals()[f"df{i}"],Vh2)).iloc[:,:2]
-    plt.scatter(PCS[0],PCS[1], c = colors[i])
+df4_ = np.array(df4-mu)
+PCS4 = pd.DataFrame(np.matmul(df4_,Vh)).iloc[:,:2]
+plt.scatter(PCS4[0],PCS4[1], c = colors[0])
 
+df3_ = np.array(df3-mu)
+PCS3 = pd.DataFrame(np.matmul(df3_,Vh)).iloc[:,:2]
+plt.scatter(PCS3[0],PCS3[1], c = colors[1])
 
-plt.legend(["UR5_2","UR5_4","UR5_3","UR5_1"])
+df2_ = np.array(df2-mu)
+PCS2 = pd.DataFrame(np.matmul(df2_,Vh)).iloc[:,:2]
+plt.scatter(PCS2[0],PCS2[1], c = colors[2])
+
+df1_ = np.array(df1-mu)
+PCS1 = pd.DataFrame(np.matmul(df1_,Vh)).iloc[:,:2]
+plt.scatter(PCS1[0],PCS1[1], c = colors[3])
+
+plt.legend(["UR5_4","UR5_3","UR5_2","UR5_1"])
 #############################################################
 # projecting on UR5_3 PC
 #############################################################
 fig()
-plt.title(f'Projection on 2 PC of UR5_3_X',fontsize = 20)
+plt.title(f'Projection on 2 PC of UR5_1_X',fontsize = 20)
 
-plt.scatter(PCA1[0],PCA1[1], c = colors[1])
+mu = np.mean(df1, axis = 0)
+df2_ = np.array(df1-mu)
+_,_,Vh = np.linalg.svd(df1_)
 
-for i,c in zip([0,2,3], "bkg"):
-    PCS = pd.DataFrame(np.matmul(globals()[f"df{i}"],Vh1)).iloc[:,:2]
-    plt.scatter(PCS[0],PCS[1], c = colors[i])
+df4_ = np.array(df4-mu)
+PCS4 = pd.DataFrame(np.matmul(df4_,Vh)).iloc[:,:2]
+plt.scatter(PCS4[0],PCS4[1], c = colors[0])
 
+df3_ = np.array(df3-mu)
+PCS3 = pd.DataFrame(np.matmul(df3_,Vh)).iloc[:,:2]
+plt.scatter(PCS3[0],PCS3[1], c = colors[1])
 
-plt.legend(["UR5_3","UR5_4","UR5_2","UR5_1"])
+df2_ = np.array(df2-mu)
+PCS2 = pd.DataFrame(np.matmul(df2_,Vh)).iloc[:,:2]
+plt.scatter(PCS2[0],PCS2[1], c = colors[2])
 
+df1_ = np.array(df1-mu)
+PCS1 = pd.DataFrame(np.matmul(df1_,Vh)).iloc[:,:2]
+plt.scatter(PCS1[0],PCS1[1], c = colors[3])
 
-# df = df_UR5_x.iloc[:-1,:6].transpose()
-# x = StandardScaler().fit_transform(df.iloc[:,:-1])
+plt.legend(["UR5_4","UR5_3","UR5_2","UR5_1"])
 
 
 #############################################################
@@ -131,41 +204,41 @@ plt.legend(["UR5_3","UR5_4","UR5_2","UR5_1"])
 # pca = PCA(n_components=2)
 # principalComponents = pca.fit_transform(x)
 # principalComponents = pca.fit_transform(UR5_4)
-principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.set_xlabel('Principal Component 1', fontsize = 15)
-ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_title('2 component PCA with X from sklearn', fontsize = 20)
-colors = ['r', 'g', 'b', 'k']
-ax.scatter(principalDf['PC1'], principalDf['PC2'], c = 'r', s = 50)
-
-
-for target, color in zip(targets,colors):
-    indicesToKeep = df_UR5.iloc[:,-1] == target
-    ax.scatter(principalDf.loc[indicesToKeep, 'PC1'], principalDf.loc[indicesToKeep, 'PC2'], c = color, s = 50)
-ax.legend(targets)
-ax.grid()
-
-#############################################################
-# 3 Principal components
-#############################################################
-pca = PCA(n_components=3)
-principalComponents = pca.fit_transform(x)
-principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2','PC3'])
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.set_xlabel('Principal Component 1', fontsize = 15)
-ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_zlabel('Principal Component 3', fontsize = 15)
-ax.set_title('3 component PCA with XYZ', fontsize = 20)
-colors = ['r', 'g', 'b', 'k']
-
-for target, color in zip(targets,colors):
-    indicesToKeep = df_UR5.iloc[:,-1] == target
-    ax.scatter(principalDf.loc[indicesToKeep, 'PC1'], principalDf.loc[indicesToKeep, 'PC2'],
-               principalDf.loc[indicesToKeep, 'PC3'], c = color, s = 50)
-ax.legend(targets)
+# principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# ax.set_xlabel('Principal Component 1', fontsize = 15)
+# ax.set_ylabel('Principal Component 2', fontsize = 15)
+# ax.set_title('2 component PCA with X from sklearn', fontsize = 20)
+# colors = ['r', 'g', 'b', 'k']
+# ax.scatter(principalDf['PC1'], principalDf['PC2'], c = 'r', s = 50)
+#
+#
+# for target, color in zip(targets,colors):
+#     indicesToKeep = df_UR5.iloc[:,-1] == target
+#     ax.scatter(principalDf.loc[indicesToKeep, 'PC1'], principalDf.loc[indicesToKeep, 'PC2'], c = color, s = 50)
+# ax.legend(targets)
+# ax.grid()
+#
+# #############################################################
+# # 3 Principal components
+# #############################################################
+# pca = PCA(n_components=3)
+# principalComponents = pca.fit_transform(x)
+# principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2','PC3'])
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# ax.set_xlabel('Principal Component 1', fontsize = 15)
+# ax.set_ylabel('Principal Component 2', fontsize = 15)
+# ax.set_zlabel('Principal Component 3', fontsize = 15)
+# ax.set_title('3 component PCA with XYZ', fontsize = 20)
+# colors = ['r', 'g', 'b', 'k']
+#
+# for target, color in zip(targets,colors):
+#     indicesToKeep = df_UR5.iloc[:,-1] == target
+#     ax.scatter(principalDf.loc[indicesToKeep, 'PC1'], principalDf.loc[indicesToKeep, 'PC2'],
+#                principalDf.loc[indicesToKeep, 'PC3'], c = color, s = 50)
+# ax.legend(targets)
 
