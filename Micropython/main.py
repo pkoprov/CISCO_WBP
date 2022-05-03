4055830cc9ee8d9ea3fe594c86328f36e6607e73
Pavel "Pasha" Koprov <84463424+pkoprov@users.noreply.github.com>
2022-05-03 18:25:45 -0400

    now data is continiously monitored

--- Micropython/main.py
+++ Micropython/main.py
@@ -14,21 +14,26 @@ for i in range(7):
 mean = sum(calibration_data) / len(calibration_data)
 sd = (sum([(i - mean) ** 2 for i in calibration_data]) / len(calibration_data)) ** 0.5
 
-duration = float(sys.stdin.readline())
+start_flag = False
+n = 0  # counter of datapoints below threshold
 
 led.value(0)
 # main loop
 while True:
     # read the input
     ax, ay, az = m.acceleration
-    if ax > mean + 5 * sd or ax < mean - 5 * sd:
-        start_time = time.ticks_ms() / 1000
+    if ax > mean + 5 * sd or ax < mean - 5 * sd and not start_flag:
+        #         start_time = time.ticks_ms()/1000
+        start_flag = True
         led.value(1)
+        n = 0
+    else:
+        n += 1
+        if n >= 100:
+            start_flag = False
+            led.value(0)
 
-        while time.ticks_ms() / 1000 - start_time < duration:
-            if m.acceleration[0] == ax and m.acceleration[1] == ay and m.acceleration[2] == az:
-                continue
-            ax, ay, az = m.acceleration
-            print(ax, ay, az)
-        led.value(0)
-        time.sleep(2)
+    if start_flag:
+        if m.acceleration[0] == ax and m.acceleration[1] == ay and m.acceleration[2] == az:
+            continue
+        print(ax, ay, az)
