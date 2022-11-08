8124d3764211090643cb5c7ed785224491a23dd4
Pavel Koprov <pkoprov@ncsu.edu>
2022-11-07 21:21:38 -0500

    change the name of samples

--- Collect vibration data.py
+++ Collect vibration data.py
@@ -1,43 +1,90 @@
+import time
+from mpu9250_jmdev.registers import *
+from mpu9250_jmdev.mpu_9250 import MPU9250
 import matplotlib.pyplot as plt
 import pandas as pd
-import serial
-from serial.tools.list_ports import comports
+import os
+from datetime import datetime
 
+mpu = MPU9250(
+    
+    address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
+    address_mpu_slave=None, 
+    bus=1,
+    gfs=GFS_1000, 
+    afs=AFS_8G, 
+    mfs=AK8963_BIT_16, 
+    mode=AK8963_MODE_C100HZ)
 
-for n, port in enumerate(comports()):
-    print(f"{n}.", port[1])
+mpu.configure() # Apply the settings to the registers.
 
-while True:
-    try:
-        port_num = int(input("Pick the port#: "))
-        port = comports()[port_num]
-        print(f"Chosen port is {port[1]}")
-        break
-    except ValueError as er:
-        print(er)
-        print("Enter integer")
+print("Start Calibrating")
+# collect an ambient vibration
+calibration_data = []
+start = time.time()
+while time.time() - start < 2:
+    calibration_data.append(mpu.readAccelerometerMaster()[0])
 
-# ser = serial.Serial("/dev/ttyACM1", baudrate=115200, timeout=0.1)
-ser = serial.Serial(port[0], baudrate=115200, timeout=0.1)
+# calculate the mean and SD of ambient vibration
+mean = sum(calibration_data) / len(calibration_data)
+sd = (sum([(i - mean) ** 2 for i in calibration_data]) / len(calibration_data)) ** 0.5
 
-n = 1
-name = "Kernel1"
-for i in range(n):
-    try:
-        ser.flushInput()
-        while True:
-            dat = ser.readall().decode().split('\r\n')
-            if dat[0] and len(dat) > 1000:
-                dat = [list(map(float, i.split(' '))) for i in dat[:-100]]
-                #             print(len(dat))
-                df = pd.DataFrame(dat)
-                df.to_csv(f"./Kernels/{name}{i}.csv")
+print("Calibration is done")
+
+def measurement_steady():
+    start_flag = False
+    data = []
+        
+    while True:
+        # read the input
+        ax, ay, az = mpu.readAccelerometerMaster()
+        # check if the values are exceeding the threshold
+        if not start_flag:
+            if ax > mean + 4 *sd or ax < mean - 4 *sd:
+                start_flag = True
+                print("Start reading")
+            else:
+                continue
+
+        data.append([ax, ay, az])
+        if not (ax > mean + 4*sd or ax < mean - 4*sd):
+            n += 1
+            if n == 100:
+                print("Stop reading")
                 break
+        else:
+            n = 0
+    return (data)
 
-        plt.plot(range(df.shape[0]), df.iloc[:, 0])
 
-        print(f"Created {name}{i}")
-    except:
-        n = 6
-plt.savefig(f"./Kernels/{name}{i}.png")
+n = int(input("How many times? "))
+i = 1
+duration = int(input("Approximate duration of move in sec? "))
 
+asset_name = input("Name of printer? ")
+try:
+    os.mkdir(f'./data/Kernels/{datetime.now().date().strftime("%Y_%m_%d")}')
+except FileExistsError:
+    pass
+try:
+    os.mkdir(f'./data/Kernels/{datetime.now().date().strftime("%Y_%m_%d")}/{asset_name}')
+except FileExistsError:
+    pass
+folder = f'./data/Kernels/{datetime.now().date().strftime("%Y_%m_%d")}/{asset_name}'
+
+while i < n+1:
+    print(f"Collecting sample {i}")
+    sample_name = f"{asset_name}_{round(time.time())}"
+    data = measurement_steady()
+    df = pd.DataFrame(data[:-100])
+    if len(df) < 500*duration:
+        continue
+        
+    df.to_csv(f"{folder}/{sample_name}.csv")
+    plt.plot(data)
+    plt.savefig(f"{folder}/{sample_name}.svg")
+    plt.close()
+    print(f"Created {sample_name}_{i}")
+    i += 1
+
+print("Done Collecting samples")
