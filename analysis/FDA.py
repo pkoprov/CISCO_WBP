b02dc5f35e34682d7b98e283c2b4de54284b4cfb
Pavel Koprov <pkoprov@ncsu.edu>
2023-11-09 21:58:36 -0500

    optimized

--- analysis/FDA.py
+++ analysis/FDA.py
@@ -72,106 +72,80 @@ class Sample(pd.DataFrame):
         self._FData = {}
         
 
-    def top_bottom_idx(self,row = 'all', lim = 'all'):
-        ''' Property returning the indexes of top and bottom of a sample in 0.01 windows'''
-        match row:
-            case 'all':
-                row = range(self.shape[0])
-            case int():
-                row = [row]
-            case list():
-                pass
-        match lim:
-            case 'all':
-                if not self._top_bottom:
-                    self._calculate_top_bottom_idx(row, lim)
-                else:
-                    for key in ['top', 'bottom']:
-                        if key not in self._top_bottom:
-                            self._calculate_top_bottom_idx(row, key)
-                        elif not all(i in self._top_bottom[key].index for i in row):
-                            self._calculate_top_bottom_idx(row, key)
-            case _:
-                if lim not in self._top_bottom or not all(i in self._top_bottom[lim].index for i in row):
-                    self._calculate_top_bottom_idx(row, lim)
-                
+    def top_bottom_idx(self, row='all', lim='all'):
+        if row == 'all':
+            row = range(self.shape[0])
+        elif isinstance(row, int):
+            row = [row]
+        
+        if lim == 'all':
+            keys_to_process = ['top', 'bottom']
+        else:
+            keys_to_process = [lim]
+
+        for key in keys_to_process:
+            if key not in self._top_bottom or not all(i in self._top_bottom[key].index for i in row):
+                self._calculate_top_bottom_idx(row, key)
+        
         return self._top_bottom
 
-    def _calculate_top_bottom_idx(self, row = 'all', lim = 'all'):
-        ''' Function to find the indexes of top and bottom of a sample in 0.01 windows'''         
-            
+
+    def _calculate_top_bottom_idx(self, row, lim):
         df = self.numeric.loc[row]
-        
-        match lim:
-            case 'all':
-                for key in ['top', 'bottom']:
-                    self._calculate_top_bottom_idx(row, key)
-            case _:
-                print(f"Calculating {lim} 10 indices")
-                var = df[df > 0] if lim == 'top' else df[df < 0]
-                var.fillna(0, inplace=True)
-                self._top_bottom[lim] = pd.DataFrame()
-                final_n = round(var.columns[-1], 2)
+        final_n = round(df.columns[-1], 2)
+        bin_edges = np.arange(0, final_n + 0.01, 0.01)
+        bins = pd.cut(df.columns, bins=bin_edges, labels=bin_edges[:-1], include_lowest=True)
 
-                # Create a binning series using `cut` to define bins every 0.01 units
-                bin_labels = np.arange(0, final_n, 0.01)
-                # Define bin edges and labels
-                bin_edges = np.arange(0, final_n + 0.01, 0.01)  # Ensure the last bin edge is included
-                bin_labels = np.round(bin_edges[:-1], 2)  # Labels should be one less than bin edges
+        var = df.clip(lower=0) if lim == 'top' else df.clip(upper=0)
+        grouped = var.groupby(bins, axis=1, observed=True)
+        self._top_bottom[lim] = grouped.agg(lambda x: x.idxmax(axis=1) if lim == "top" else x.idxmin(axis=1))
 
-                # Create bins
-                bins = pd.cut(var.columns, bins=bin_edges, labels=bin_labels, include_lowest=True)
-                # Group by the bins and apply the idxmax or idxmin function
-                grouped = var.groupby(bins, axis = 1, observed=True)
-                self._top_bottom[lim] = grouped.agg(lambda x: x.idxmax(axis=1) if lim == "top" else x.idxmin(axis=1))
 
-    def top_bottom_filled(self, row = 'all', lim = 'all'):
-        ''' Property returning the DataFrames of top and bottom of a sample with filled values'''
+    def top_bottom_filled(self, row='all', lim='all'):
         if not self._top_bottom:
-            self._calculate_top_bottom_filled(row , lim)
+            self._calculate_top_bottom_filled(row, lim)
         return self._top_bottom_filled
 
-    def _calculate_top_bottom_filled(self, row = 'all', lim = 'all'):
-        '''Funtion to interpolate the missing x values'''
-        row = [row] if isinstance(row, int) else row
-        match lim:
-            case 'all':
-                for key in ['top', 'bottom']:
-                    self._calculate_top_bottom_filled(row, key)
-            case _:
-                if lim not in self._top_bottom or not all(i in self._top_bottom[lim].index for i in row):
-                    self.top_bottom_idx(row, lim)
+    def _calculate_top_bottom_filled(self, row, lim):
+        if row == 'all':
+            row = range(self.shape[0])
+        elif isinstance(row, int):
+            row = [row]
         
-        for key in self._top_bottom:
-            # linearly interpolate and add 0 as a starting point
-            lim = self._top_bottom[key].apply(lambda row: np.interp(self.grid, [0]+row.tolist(), [0] + self.numeric.loc[row.name,row.values].tolist()), axis=1)
-            self._top_bottom_filled[key] = pd.DataFrame(lim.values.tolist(), index=lim.index, columns=self.grid)
-        return self._top_bottom_filled
+        if lim == 'all':
+            keys_to_process = ['top', 'bottom']
+        else:
+            keys_to_process = [lim]
+
+        for key in keys_to_process:
+            if key not in self._top_bottom or not all(i in self._top_bottom[key].index for i in row):
+                self.top_bottom_idx(row, key)
+            
+            lim_df = self._top_bottom[key].apply(lambda r: np.interp(self.grid, [0] + r.tolist(), [0] + self.numeric.loc[r.name, r.values].tolist()), axis=1)
+            self._top_bottom_filled[key] = pd.DataFrame(lim_df.values.tolist(), index=lim_df.index, columns=self.grid)
+
     
 
-    def FData(self, row = 'all', lim = 'all', plot = False):
+    def FData(self, row='all', lim='all', plot=False):
+        if row == 'all':
+            row = range(self.shape[0])
+        elif isinstance(row, int):
+            row = [row]
 
-            '''Function to convert the top and bottom shapes to a FData representation'''
-            match row:
-                case 'all':
-                    row = range(self.shape[0])
-                case int():
-                    row = [row]
-                case list():
-                    pass
-            if lim not in self._top_bottom_filled or not all(i in self._top_bottom_filled[lim].index for i in row):
-                self.top_bottom_filled(row, lim)
+        if lim not in self._top_bottom_filled or not all(i in self._top_bottom_filled[lim].index for i in row):
+            self.top_bottom_filled(row, lim)
 
-            y = self._top_bottom_filled[lim].loc[row]
-            y = y.to_numpy().reshape(len(row), -1, 1)
-
-            self._FData[lim] = skfda.FDataGrid(y, grid_points=self.grid)
+        for key in ['top', 'bottom'] if lim == 'all' else [lim]:
+            print(f"Converting {key} {row} to FData")
+            y = self._top_bottom_filled[key].loc[row].to_numpy().reshape(len(row), -1, 1)
+            self._FData[key] = skfda.FDataGrid(y, grid_points=self.grid)
+            
             if plot:
-                self._FData[lim].plot(axes = plt.gca(), alpha = 0.5)
-                plt.legend(self._top_bottom_filled[lim].index)
-                plt.title(f"{lim} shapes")
-                   
-            return self._FData
+                self._FData[key].plot(axes=plt.gca(), alpha=0.5)
+                plt.legend(self._top_bottom_filled[key].index)
+                plt.title(f"{key} shapes")
+
+        return self._FData
 
 
 def plot_FPCA_results(sample, labels, label, train_ind, test_ind, y_test, test_errors_fpca, train_errors_fpca):
@@ -192,17 +166,6 @@ def plot_FPCA_results(sample, labels, label, train_ind, test_ind, y_test, test_e
 
     plt.pause(0.5)
 
-
-def calculate_errors(train_set_basis, test_set_basis, fpca_clean):
-    train_scores = fpca_clean.transform(train_set_basis)
-    test_scores = fpca_clean.transform(test_set_basis)
-    train_set_hat_basis = fpca_clean.inverse_transform(train_scores)
-    test_set_hat_basis = fpca_clean.inverse_transform(test_scores)
-    print("Calculating l2 distance for test data")
-    test_errors_fpca = l2_distance(test_set_basis, test_set_hat_basis) / l2_norm(test_set_basis)
-    print("Calculating l2 distance for train data")
-    train_errors_fpca = l2_distance(train_set_basis, train_set_hat_basis) / l2_norm(train_set_basis)
-    return test_errors_fpca,train_errors_fpca
 
 if __name__ == "__main__":
     data = pd.read_csv(r'data\Kernels\2023_02_07\Prusa_merged.csv')
