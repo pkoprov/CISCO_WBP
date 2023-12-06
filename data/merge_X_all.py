788b1c0bd48077067cb08698e3f3d1c5501386bc
Pavel Koprov <pkoprov@ncsu.edu>
2023-12-06 17:41:50 -0500

    add function folders_to_process()

--- data/merge_X_all.py
+++ data/merge_X_all.py
@@ -1,23 +1,30 @@
 import sys
 import os
-from datetime import datetime
+import re
 
 # Get the path of the parent directory of the current working directory
 sys.path.append( os.getcwd())
 
 from analysis.read_merge_align_write import create_dataset
 
-folders = os.listdir(r"data\Kernels")
-init_idx = folders.index("2023_11_18")
-fin_idx = folders.index(datetime.now().strftime("%Y_%m_%d"))
-folders = folders[init_idx:fin_idx+1]
+START = {"VF-2":"2023_11_18", "UR":"2023_11_21", "Bambu":"2023_11_25"}
 
-for folder in folders:
-    print(os.path.join(r"data\Kernels",folder))
-    for subf in os.listdir(os.path.join(r"data\Kernels",folder)):
-        goal = os.path.join(r"data\Kernels",folder,subf)
-        if os.path.isdir(goal):
-            if os.path.exists(os.path.join(goal,"merged_X.csv")):
-                print("already exists")
-                continue
-            create_dataset(goal)
+def folders_to_process(typ):
+    date_pattern = re.compile(r"\d{4}_\d{2}_\d{2}")
+    folder_list = [d for d in os.listdir(r".\data\Kernels") if date_pattern.match(d)]
+    init_idx = folder_list.index(START[typ])
+    fin_idx = len(folder_list)
+    return folder_list[init_idx:fin_idx]
+
+
+if __name__ == "__main__":
+    folders = folders_to_process("VF-2")
+    for folder in folders:
+        print(os.path.join(r"data\Kernels",folder))
+        for subf in os.listdir(os.path.join(r"data\Kernels",folder)):
+            goal = os.path.join(r"data\Kernels",folder,subf)
+            if os.path.isdir(goal):
+                if os.path.exists(os.path.join(goal,"merged_X.csv")):
+                    print(subf, "already exists")
+                    continue
+                create_dataset(goal)
