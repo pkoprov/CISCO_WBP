511a9452e9adc637e667cda45569c65defa718b9
Pavel Koprov <pkoprov@ncsu.edu>
2023-12-09 20:59:20 -0500

    replace os.path.join(r"data\Kernels", folder) with `path`

--- data/train_models.py
+++ data/train_models.py
@@ -8,8 +8,9 @@ from analysis.testing_models import train_model
 if __name__ == "__main__":
     folders = folders_to_process("VF-2")
     for folder in folders:
-        print(os.path.join(r"data\Kernels", folder))
-        for subf in [subf for subf in os.listdir(os.path.join(r"data\Kernels", folder)) if os.path.isdir(os.path.join(r"data\Kernels", folder, subf))]:
+        path = os.path.join(r"data\Kernels", folder)
+        print(path)
+        for subf in [subf for subf in os.listdir(path) if os.path.isdir(os.path.join(path, subf))]:
             print(subf)
             if "VF" in subf:
                 typ = "VF-2"
@@ -21,8 +22,8 @@ if __name__ == "__main__":
                 print("Unknown type")
                 typ = input("Type the asset type:\n>>")
 
-            path = os.path.join(r"data\Kernels", folder, subf, f"{subf}_merged_new.csv") if folder != START[typ] else os.path.join(r"data\Kernels", folder, subf,"merged_X.csv")
-            if sum([True for model in os.listdir(os.path.dirname(path)) if "_fpca.pkl" in model]) == 2:
+            model_path = os.path.join(path, subf, f"{subf}_merged_new.csv") if folder != START[typ] else os.path.join(path, subf,"merged_X.csv")
+            if sum([True for model in os.listdir(os.path.dirname(model_path)) if "_fpca.pkl" in model]) == 2:
                 print(subf, "model already exists")
                 continue
-            train_model(path)
+            train_model(model_path)
